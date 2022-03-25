87afbb886e8a1201f1ec13aaf5fc5e358eb553ae
Josep Vicent Bellver <josevbellver@gmail.com>
2022-03-25 16:05:43 +0100

    arreglao4

--- carelink.js
+++ carelink.js
@@ -14,8 +14,8 @@ var logger = require('./logger');
 var MMCONNECT_SERVER = process.env['MMCONNECT_SERVER'];
 var CARELINK_EU = MMCONNECT_SERVER === 'EU';
 var MMCONNECT_SERVERNAME = process.env['MMCONNECT_SERVERNAME'];
-//var DEFAULT_CARELINKSERVERADDRESS = MMCONNECT_SERVERNAME || (CARELINK_EU ? "carelink.minimed.eu" : "carelink.minimed.com");
-var DEFAULT_CARELINKSERVERADDRESS = "carelink.minimed.com";
+var DEFAULT_CARELINKSERVERADDRESS = MMCONNECT_SERVERNAME || (CARELINK_EU ? "carelink.minimed.eu" : "carelink.minimed.com");
+//var DEFAULT_CARELINKSERVERADDRESS = "carelink.minimed.com";
 
 var DEFAULT_COUNTRYCODE = process.env['MMCONNECT_COUNTRYCODE'] || 'gb';
 var DEFAULT_LANGCODE = process.env['MMCONNECT_LANGCODE'] || 'en';
